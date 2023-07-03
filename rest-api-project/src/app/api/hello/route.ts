12c6d15455d16a04c96dd51f711223297f8e1201
alpari <alpari@ttu.ee>
2023-07-03 17:59:07 +0300

    Fifth project 2nd part

---
+++ rest-api-project/src/app/api/hello/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+import { limiter } from '../todos/config/limiter';
+
+export async function GET(request: Request) {
+  const remaining = await limiter.removeTokens(1);
+  console.log('remaining: ', remaining);
+
+  const origin = request.headers.get('origin');
+
+  if (remaining < 0) {
+    return new NextResponse(null, {
+      status: 429,
+      statusText: 'Too Many Requests',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type'
+      },
+    });
+  }
+
+  return new Response('Hello, World!');
+}
